9bfe5ca53fd2a8e7b169301ff87f00d0996d18cb
sonali <sonalikumariram06@gmail.com>
2022-10-20 23:49:35 +0530

    first commit

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroFormComponent  } from './hero-form.component';
+
+describe('FormComponent', () => {
+  let component: HeroFormComponent;
+  let fixture: ComponentFixture<HeroFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeroFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeroFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
